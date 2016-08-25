9235cbcb399cd26807b6a252b9521be778327d64
bshankar <ebhavanishankar@gmail.com>
2016-08-25 17:16:46 +0530

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+# start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx -- -keeptty
+    end
+end
+
+# set editor to nvim
+set -x EDITOR nvim
+set -x BROWSER firefox
+set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+alias vim=/usr/bin/nvim
+alias clang++="/usr/bin/clang++ -std=c++11"
