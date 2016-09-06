2fba366ea624fd69a38bbb0fef44a5702f147b5c
bshankar <ebhavanishankar@gmail.com>
2016-09-06 16:29:01 +0530

    Move weechat configuration folder to .config/weechat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ end
 # set editor to nvim
 set -x EDITOR nvim
 set -x BROWSER firefox
+set -x WEECHAT_HOME $HOME/.config/weechat
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 alias vim=/usr/bin/nvim
 alias clang++="/usr/bin/clang++ -std=c++11"
