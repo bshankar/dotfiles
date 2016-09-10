02609ceb50c531a5f3dac197058016702de167cb
bshankar <ebhavanishankar@gmail.com>
2016-09-10 18:16:59 +0530

    make python find _lldb module using a temporary hack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,10 @@ set -x EDITOR nvim
 set -x BROWSER firefox
 set -x WEECHAT_HOME $HOME/.config/weechat
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+
+# Hack to make python2 detect lldb
+set -x PYTHONPATH /usr/lib/python2.7/site-packages/lldb
+
 alias vim=/usr/bin/nvim
 alias clang++="/usr/bin/clang++ -std=c++11"
 alias tmux="tmux -f ~/.config/tmux/tmux.conf"
