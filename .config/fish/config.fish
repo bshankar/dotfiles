ec8547f4a6260c759f44e7e64c1f01a055cfd1fb
bshankar <ebhavanishankar@gmail.com>
2016-09-15 10:49:37 +0530

    Run the jvm though drip instead of java command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ set -x EDITOR nvim
 set -x BROWSER firefox
 set -x WEECHAT_HOME $HOME/.config/weechat
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+set -x JAVACMD '/usr/bin/drip'
 set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 
 # Hack to make python2 detect lldb
