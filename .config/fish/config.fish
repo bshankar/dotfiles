c04cac90b62e573b4516b8e7d0c5895913e7d71e
bshankar <ebhavanishankar@gmail.com>
2016-09-19 12:35:23 +0530

    Use an alias for java instead of _JAVA_OPTIONS which give an annoying message each time java command is run

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,6 @@ set -x EDITOR nvim
 set -x BROWSER firefox
 set -x WEECHAT_HOME $HOME/.config/weechat
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
-set -x _JAVA_OPTIONS '-Xms512M -Xmx2G -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 
 # Hack to make python2 detect lldb
 set -x PYTHONPATH /usr/lib/python2.7/site-packages/lldb
@@ -18,3 +17,4 @@ set -x PYTHONPATH /usr/lib/python2.7/site-packages/lldb
 alias vim=/usr/bin/nvim
 alias clang++="/usr/bin/clang++ -std=c++11"
 alias tmux="tmux -f ~/.config/tmux/tmux.conf"
+alias java='java -Xms512M -Xmx2G -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
