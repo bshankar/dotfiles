3f06915e9d08e41f5f81abd4818f5fba559c7309
Bhavani Shankar E <ebhavanishankar@gmail.com>
2019-02-26 11:25:37 +0530

    Add new useful aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,15 @@ function fish_title
   true
 end
 
+function fish_greeting
+  true
+end
+
 # set editor to nvim
 set -x EDITOR nvim
 set -x BROWSER firefox-developer-edition
+
+keychain -q --agents gpg,ssh; keychain -q --eval id_ed25519
 
 alias vim=/usr/bin/nvim
 alias diff="/usr/bin/diff --color"
@@ -20,8 +26,11 @@ alias ls="exa"
 
 alias kp="kubectl --context=adori-prod"
 alias kd="kubectl --context=adori-dev"
+alias kb="kubectl --context=adori-beta"
+
 alias kup="kubectl config use-context adori-prod"
 alias kud="kubectl config use-context adori-dev"
+alias kud="kubectl config use-context adori-beta"
 
 alias kpb="kp apply -f /home/ebs/Documents/code/adori/infra/prod/backend-v5.yaml"
 alias kdb="kd apply -f /home/ebs/Documents/code/adori/infra/dev/backend-v5.yaml"
