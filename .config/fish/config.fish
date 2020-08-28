7f22b1074c158f78ec855e8efcd613644288d628
Bhavani Shankar <ebhavanishankar@gmail.com>
2020-08-28 12:18:07 +0530

    Use sccache to cache compilation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,13 @@ end
 set -x EDITOR nvim
 set -x BROWSER firefox
 set -gx PATH /home/ebs/.cargo/bin /home/ebs/.emacs.d/bin $PATH
-set -x RUSTC_WRAPPER=/usr/bin/sccache
+set -x RUSTC_WRAPPER /usr/bin/sccache
+
 alias vim=/usr/bin/nvim
+alias gcc='/usr/bin/sccache /usr/bin/gcc'
+alias cc='/usr/bin/sccache /usr/bin/gcc'
+alias g++='/usr/bin/sccache /usr/bin/g++'
+alias c++='/usr/bin/sccache /usr/bin/g++'
 
 starship init fish | source
 zoxide init fish | source
