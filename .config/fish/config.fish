504a4592606ed444c88dff9042e88b23e97d52ef
Bhavani Shankar <ebhavanishankar@gmail.com>
2020-08-28 12:07:45 +0530

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ end
 set -x EDITOR nvim
 set -x BROWSER firefox
 set -gx PATH /home/ebs/.cargo/bin /home/ebs/.emacs.d/bin $PATH
+set -x RUSTC_WRAPPER=/usr/bin/sccache
 alias vim=/usr/bin/nvim
 
 starship init fish | source
